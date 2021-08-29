55e898aa675c3dbf567ed3021fa0d149c9f9ca50
Louis Vela <lvela@doximity.com>
2021-08-29 15:28:39 -0500

    Add code to open gate

--- app/controllers/gate_controller.rb
+++ app/controllers/gate_controller.rb
@@ -2,6 +2,8 @@
 
 class GateController < ApplicationController
   def open
+    result = `curl https://api.particle.io/v1/devices/#{ENV["PROTON_DEVICE_ID"]}/press -d access_token=#{ENV["PROTON_ACCESS_TOKEN"]}`
+    puts result
 
     redirect_to root_path
   end
